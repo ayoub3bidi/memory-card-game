e75575533de0472d8db1835d1f39ff9fb855e783
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-03 09:22:57 +0100

    ✅ Creating the game board

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
 <h1>heloo</h1>
+<section class="game-board">
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+</section>
 </template>
 
 <script>
@@ -11,4 +29,26 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap:30px;
+  grid-row-gap:30px;
+  justify-content: center;
+
+}
 </style>
