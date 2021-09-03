533322a2d58038e09aee5a18d2286347df232e90
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-03 10:14:39 +0100

    ✅ Creating card component with value prop

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
 <h1>heloo</h1>
 <section class="game-board">
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
+  <Card v-for="(card, index) in cardList" :key="`card-${index}`" :value="card"/>
 </section>
 </template>
 
 <script>
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
-  components: {}
+  components: { Card },
+  setup() {
+    const cardList = []
+
+    for (let i = 0; i < 16; i++) {
+      cardList.push(i)
+    }
+
+    return { cardList }
+  }
 }
 </script>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="card">
+    {{value}}
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
