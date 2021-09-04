779e3a07fe0a4074fd3e16e50cdc2bdda116fe12
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-04 11:19:37 +0100

    ✅ Track user choice and validate choices

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card" @click="selectCard">
     <div v-if="visible" class="card-face is-front">
-      {{ value }}
+      {{ value }} - {{ matched }}
     </div>
     <div v-else class="card-face is-back">
       Back
@@ -12,6 +12,10 @@
 <script>
 export default {
   props: {
+    matched: {
+      type: Boolean,
+      default: false
+    },
     position: {
       type: Number,
       required: true
@@ -28,7 +32,8 @@ export default {
   setup(props, context) {
     const selectCard = () => {
       context.emit('select-card', {
-        position: props.position
+        position: props.position,
+        faceValue: props.value
       })
     }
     return {
